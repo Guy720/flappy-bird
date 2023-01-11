92263ffbbadc73fab11f419392f77c98eb137099
@kayke.torres <kayke.torres@ampliato.com.br>
2023-01-11 17:35:24 -0300

    Implementação de coalisao, pulo, som ao coalidir com o chão

--- jogo.js
+++ jogo.js
@@ -1,4 +1,7 @@
-console.log('[DevSoutinho] Flappy Bird');
+
+
+const hitSong = new Audio();
+hitSong.src = './efeitos/hit.wav';
 
 const sprites = new Image();
 sprites.src = './sprites.png';
@@ -64,30 +67,58 @@ const chao = {
   },
 };
 
-// Imagem do passarinho
-const flappyBird = {
-  spriteX: 0,
-  spriteY: 0,
-  largura: 33,
-  altura: 24,
-  x: 10,
-  y: 50,
-  gravidade: 0.25,
-  velocidade: 0,
-  atualiza() {
-    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
-    flappyBird.y = flappyBird.y + flappyBird.velocidade;
-  },
-  desenha() {
-    contexto.drawImage(
-      sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-      flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura,
-    );
+function fazColisao(flappyBird, chao){
+  const flappyBirdY = flappyBird.y + flappyBird.altura;
+  const chaoY = chao.y;
+
+  if (flappyBirdY >= chaoY) {
+    return true;
   }
+    return false;
 }
+
+function criaFlappyBird (){
+  // Imagem do passarinho
+  const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+    pulo: 4.6,
+    pula(){
+      flappyBird.velocidade = - flappyBird.pulo;
+    },
+    gravidade: 0.25,
+    velocidade: 0,
+    atualiza() {
+      if (fazColisao(flappyBird, chao)) {
+        hitSong.play();
+
+        setTimeout(() => {
+          mudaParaTela(Telas.INICIO);
+        }, 500);
+        return;
+      }
+
+      flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+      flappyBird.y = flappyBird.y + flappyBird.velocidade;
+    },
+    desenha() {
+      contexto.drawImage(
+        sprites,
+        flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+        flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+        flappyBird.x, flappyBird.y,
+        flappyBird.largura, flappyBird.altura,
+      );
+    }
+  }
+  return flappyBird;
+}
+
+
 
 //Tela de Início
 const mensagemGetReady = {
@@ -108,18 +139,28 @@ const mensagemGetReady = {
   }
 }
 
+const globais = {};
 let telaAtiva = {};
+
+
 function mudaParaTela(novaTela){
   telaAtiva = novaTela;
-
+  
+  if (telaAtiva.inicializa) {
+    telaAtiva.inicializa();
+  }
 }
+
 //Telas
 const Telas = {
   INICIO: {
+    inicializa(){
+      globais.flappyBird = criaFlappyBird();
+    },
     desenha(){
         planoDeFundo.desenha();
         chao.desenha();
-        flappyBird.desenha();
+        globais.flappyBird.desenha();
         mensagemGetReady.desenha();
     },
     click(){
@@ -136,10 +177,13 @@ Telas.JOGO = {
   desenha(){
     planoDeFundo.desenha();
     chao.desenha();
-    flappyBird.desenha();
+    globais.flappyBird.desenha();
+    },
+    click(){
+      globais.flappyBird.pula();
     },
     atualiza(){
-      flappyBird.atualiza();
+      globais.flappyBird.atualiza();
     }
 }
 
